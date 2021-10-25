464ba461bb97b35a5e0fe69dba2fa6e76974e57b
gmachadosimoes <58008396+gmachadosimoes@users.noreply.github.com>
2021-10-25 23:57:29 +0100

    Add project files.

---
+++ App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press any key to exit...");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executing 2nd project - variables!");
+
+            int yourAge;
+            yourAge = 34;
+
+            Console.WriteLine(yourAge);
+
+            Console.WriteLine("You are " + yourAge + ".");
+
+            Console.WriteLine("End of program, press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
